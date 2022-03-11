2ea50db2487e2474aa2ff009d37abaf610c7498e
Sergey <sergeychits20@gmail.com>
2022-03-10 22:57:54 -0700

    improved load world method, added catch for general error, removed print statements

--- src/mvh/util/Reader.java
+++ src/mvh/util/Reader.java
@@ -33,66 +33,39 @@ public final class Reader {
                 String[] lineObjects = line.split(",");
                 int numOfElementsInLine = lineObjects.length;
 
-                if (numOfElementsInLine == 2){
-                    System.out.println("floor");
-                }
-                if (numOfElementsInLine == 6){
-                    System.out.println("monster");
+                if (numOfElementsInLine > 2) {
                     int row = Integer.parseInt(lineObjects[0]);
                     int column = Integer.parseInt(lineObjects[1]);
                     String type = lineObjects[2];
-
                     char symbol = lineObjects[3].charAt(0);
                     int health = Integer.parseInt(lineObjects[4]);
 
-                    char weaponTypeChar = lineObjects[5].charAt(0);
-                    WeaponType weaponType = WeaponType.getWeaponType(weaponTypeChar);
-                    Monster monster = new Monster(health, symbol, weaponType);
-                    world.addEntity(row, column, monster);
-                    System.out.println(row + column + type + symbol + health + weaponType);
+                    if (type.equals("MONSTER")) {
+                        char weaponTypeChar = lineObjects[5].charAt(0);
+                        WeaponType weaponType = WeaponType.getWeaponType(weaponTypeChar);
+                        Monster monster = new Monster(health, symbol, weaponType);
+                        world.addEntity(row, column, monster);
 
-                }
-                if (numOfElementsInLine == 7){
-                    System.out.println("hero");
-                    int row = Integer.parseInt(lineObjects[0]);
-                    int column = Integer.parseInt(lineObjects[1]);
-                    String type = lineObjects[2];
+                    }
 
-                    char symbol = lineObjects[3].charAt(0);
-                    int health = Integer.parseInt(lineObjects[4]);
-
-                    int weaponStrength = Integer.parseInt(lineObjects[5]);
-                    int armorStrength = Integer.parseInt(lineObjects[6]);
-                    Hero hero = new Hero(health, symbol, weaponStrength, armorStrength);
-                    //Hero(int health, char symbol, int weaponStrength, int armorStrength)
-                    world.addEntity(row, column, hero);
+                    if (type.equals("HERO")) {
+                        int weaponStrength = Integer.parseInt(lineObjects[5]);
+                        int armorStrength = Integer.parseInt(lineObjects[6]);
+                        Hero hero = new Hero(health, symbol, weaponStrength, armorStrength);
+                        world.addEntity(row, column, hero);
+                    }
                 }
 
                 lineNum--;
 
             }
-//            int lineNumber = 0;
-//            int rows = 0;
-//            int columns = 0;
-//            while (fileReader.hasNextLine()) {
-//                String line = fileReader.nextLine();
-//                if (lineNumber == 0){
-//                    rows = Integer.parseInt(line);
-//                }
-//
-//                if (lineNumber == 1){
-//                    columns = Integer.parseInt(line);
-//                }
-//                System.out.println(line);
-//                lineNumber++;
-//            }
-//            fileReader.close();
-            // World world = new World(rows, columns);
-            //return world;
-
 
         } catch (FileNotFoundException error) {
             System.out.println("File was not found.");
+            error.printStackTrace();
+            System.exit(1);
+        } catch (Exception error){
+            System.out.println("Unexpected error happened while loading world.");
             error.printStackTrace();
             System.exit(1);
         }
